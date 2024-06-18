ffa3480c28938d7204bf33ed331d5fed4270dad4
TheoryMan <gustavo.olicardoso07@gmail.com>
2024-06-18 17:12:53 -0300

    config

--- backproject/src/Loaders/Mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-async function startDB() {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("Banco de Dados Inicializado");
-}
-module.exports = startDB;
--- backproject/src/Loaders/index.js
+++
@@ -1,8 +0,0 @@
-const startDB = require("./Mongodb");
-
-class Loaders {
-  start() {
-    startDB();
-  }
-}
-module.exports = new Loaders();
